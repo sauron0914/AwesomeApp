3826682c66ff7926dcb919c7e7061e2ad2d00b08
jpjia <jianpingjia@xiaohongshu.com>
2020-03-25 11:48:46 +0800

    添加alias配置

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -7,19 +7,19 @@
  */
 
 import React from 'react';
-import {Scene, Router} from 'react-native-router-flux';
+import { Scene, Router } from 'react-native-router-flux';
 import HomeDashboard from './Home/Dashboard/Dashboard';
-import HomeList from './Home/List/List';
+import HomeList from '@views/Home/List/List';
 
 const App: React.FC = () => {
-    return (
-        <Router>
-            <Scene key="root">
-                <Scene key="dashboard" component={HomeDashboard} />
-                <Scene key="home-list" component={HomeList} />
-            </Scene>
-        </Router>
-    );
+  return (
+    <Router>
+      <Scene key="root">
+        <Scene key="dashboard" component={HomeDashboard} />
+        <Scene key="home-list" component={HomeList} />
+      </Scene>
+    </Router>
+  );
 };
 
 export default App;
